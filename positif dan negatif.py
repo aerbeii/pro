f51f89a648e161ffc3d07a063d8007784a651850
aerbeii <arbiyt057@gmail.com>
2024-10-25 14:06:19 +0700

    Add files via upload

---
+++ positif dan negatif.py
@@ -0,0 +1,12 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+angka = float(input("Masukkan nilai angka: "))
+
+if angka > 0:
+    print("angka adalah positif.")
+elif angka < 0:
+    print("angka adalah negatif.")
+else:
+    print("angka adalah nol.")
+
